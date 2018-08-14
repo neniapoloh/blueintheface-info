88385dd509b44b1da0949de7e3653ecfc96886bb
neniapoloh <42388411+neniapoloh@users.noreply.github.com>
2018-08-14 21:38:39 +0300

    Create info.js

---
+++ info.js
@@ -0,0 +1,62 @@
+{
+  info: [
+    {
+        index: '',
+        text: 'BOOKING',
+        link: 'mailto:blueintheface.band@gmail.com?subject=Book%20Blueintheface'
+    },
+    {
+        index: '1808',
+        text: 'Bar Zoom, Hyvinkää FIN'
+    },
+    {
+        index: '1908',
+        text: 'Maanalainen, Tampere FIN'
+    },
+    {
+        index: '2208',
+        text: 'Lepakkomies, Helsinki FIN'
+    },
+    {
+        index: '2308',
+        text: 'Fontaine Palace, Liepja LAT'
+    },
+    {
+        index: '2408',
+        text: 'Przychodnia Skłot, Warszawa POL'
+    },
+    {
+        index: '2508',
+        text: 'Rote Insel, Berlin GER'
+    },
+    {
+        index: '2608',
+        text: 'Vinárna pod veží, Horazdovic CZE'
+    },
+    {
+        index: '2708',
+        text: 'Kulturák klub, Bratislava SVK'
+    },
+    {
+        index: '2808',
+        text: 'Hájovňa, Žilina SVK'
+    },
+    {
+        index: '2908',
+        text: 'Cult Club, Banská Bystrica SLV'
+    },
+    {
+        index: '3008',
+        text: 'Protestacja, Lodz POL'
+    },   
+    {
+        index: '0109',
+        text: 'Depo, Riga LAT'
+    },
+    {
+        index: '',
+        text: 'CONTACT BAND',
+        link: 'mailto:blueintheface.band@gmail.com?subject=Contact%20Blueintheface'
+    }
+  ]
+}
